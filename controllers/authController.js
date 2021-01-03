19b3e6c54a6bb35b90c7e1d0885b7ded2b753b82
Mateusz Rusak <rusakkk@live.com>
2021-01-03 16:30:11 +0100

    fix cookie expiration time

--- controllers/authController.js
+++ controllers/authController.js
@@ -16,7 +16,7 @@ const createSendToken = (user, statusCode, req, res) => {
 
     const cookieOptions = {
         expires: new Date(
-            Date.now() + (process.env.JWT_EXPIRES + 24 * 60 * 60 * 1000)
+            Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000
         ),
         httpOnly: true,
         secure: req.secure || req.headers['x-forwarded-proto'] === 'https',
