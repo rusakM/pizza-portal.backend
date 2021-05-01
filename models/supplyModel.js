51dc06583fa70b81d02f20054260c36e4c0a84cb
Mateusz Rusak <rusakkk@live.com>
2021-05-01 15:54:05 +0200

    add slug to ingredient, and isOnTop field

--- models/supplyModel.js
+++ models/supplyModel.js
@@ -1,26 +1,41 @@
 const mongoose = require('mongoose');
+const slugify = require('slugify');
 
-const supplySchema = new mongoose.Schema({
-    name: {
-        type: String,
-        required: [true, 'Podaj nazwę składnika'],
-        unique: true,
-        trim: true,
+const supplySchema = new mongoose.Schema(
+    {
+        name: {
+            type: String,
+            required: [true, 'Podaj nazwę składnika'],
+            unique: true,
+            trim: true,
+        },
+        price: {
+            type: Number,
+            required: [true, 'Podaj cenę składnika'],
+            min: 0,
+        },
+        isAwailable: {
+            type: Boolean,
+            default: true,
+            select: false,
+        },
+        coverPhoto: {
+            type: String,
+            default: 'default.jpg',
+        },
+        isOnTop: {
+            type: Boolean,
+            default: false,
+        },
     },
-    price: {
-        type: Number,
-        required: [true, 'Podaj cenę składnika'],
-        min: 0,
-    },
-    isAwailable: {
-        type: Boolean,
-        default: true,
-        select: false,
-    },
-    coverPhoto: {
-        type: String,
-        default: 'default.jpg',
-    },
+    {
+        toJSON: { virtuals: true },
+        toObject: { virtuals: true },
+    }
+);
+
+supplySchema.virtual('slug').get(function () {
+    return slugify(this.name, { lower: true });
 });
 
 const Supply = new mongoose.model('Supply', supplySchema);
